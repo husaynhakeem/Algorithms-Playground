ef019f6d0fae2f2aa9eabeb7d8a55fc8b55598e2
husaynhakeem@gmail.com <husaynhakeem@gmail.com>
2018-04-14 16:32:01 -0700

    Chapter 2 exercise 6: Solution 3

---
+++ src/chapter2_exercise6/java/Solution_3.java
@@ -0,0 +1,39 @@
+package chapter2_exercise6.java;
+
+import Utilities.java.LinkedListUtils.Node;
+
+import java.util.Stack;
+
+import static Utilities.java.LinkedListUtils.createLinkedListFrom;
+
+public class Solution_3 {
+
+    public static void main(String[] args) {
+        Node first = createLinkedListFrom(1, 2, 3, 4, 3, 2, 1);
+        System.out.println(isPalindrome(first, 7));
+
+        Node second = createLinkedListFrom(3, 4, 5, 6);
+        System.out.println(isPalindrome(second, 4));
+    }
+
+    private static boolean isPalindrome(Node node, int size) {
+        Stack<Integer> stack = new Stack<>();
+        boolean isSizeEven = size % 2 == 0;
+
+        for (int i = 0; i < size / 2; i++) {
+            stack.push(node.data);
+            node = node.next;
+        }
+
+        if (!isSizeEven)
+            node = node.next;
+
+        while (node != null) {
+            if (stack.pop() != node.data)
+                return false;
+            node = node.next;
+        }
+
+        return true;
+    }
+}
