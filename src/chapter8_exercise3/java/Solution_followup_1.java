a1ea0d5df5fb184bd706a573ab3640dc85079723
husaynhakeem@gmail.com <husaynhakeem@gmail.com>
2018-04-21 15:34:02 -0700

    Chapter 8 exercise 3 followup: Solution 1

---
+++ src/chapter8_exercise3/java/Solution_followup_1.java
@@ -0,0 +1,35 @@
+package chapter8_exercise3.java;
+
+public class Solution_followup_1 {
+
+    public static void main(String[] args) {
+        int[] a = new int[]{-40, -20, -1, 1, 2, 3, 5, 7, 9, 12, 13};
+        System.out.println(findMagicIndex(a));
+    }
+
+    private static int findMagicIndex(int[] a) {
+        if (a == null)
+            return -1;
+
+        return findMagicIndex(a, 0, a.length - 1);
+    }
+
+    private static int findMagicIndex(int[] a, int low, int high) {
+        if (high < low)
+            return -1;
+
+        int midIndex = (low + high) / 2;
+        int midValue = a[midIndex];
+
+        if (midValue == midIndex)
+            return midIndex;
+
+        int leftIndex = Math.min(midIndex - 1, midValue);
+        int magicIndex = findMagicIndex(a, low, leftIndex);
+        if (magicIndex >= 0)
+            return magicIndex;
+
+        int rightIndex = Math.max(midIndex + 1, midValue);
+        return findMagicIndex(a, rightIndex, high);
+    }
+}
