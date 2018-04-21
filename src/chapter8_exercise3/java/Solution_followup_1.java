6c7c5959ea1a1e4f6d321c18d2c4aac896767ae1
husaynhakeem@gmail.com <husaynhakeem@gmail.com>
2018-04-21 16:40:44 -0700

    Chapter 8 exercise 3 followup: Solution 1 correction

--- src/chapter8_exercise3/java/Solution_followup_1.java
+++ src/chapter8_exercise3/java/Solution_followup_1.java
@@ -3,7 +3,8 @@ package chapter8_exercise3.java;
 public class Solution_followup_1 {
 
     public static void main(String[] args) {
-        int[] a = new int[]{-40, -20, -1, 1, 2, 3, 5, 7, 9, 12, 13};
+        int[] a = new int[]{-10, -5, 2, 2, 2, 3, 4, 7, 9, 12, 13};
+        //int[] a = new int[]{0, 0, 0, 3, 6};
         System.out.println(findMagicIndex(a));
     }
 
