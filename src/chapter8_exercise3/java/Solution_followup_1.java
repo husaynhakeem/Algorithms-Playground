fbaf139433060978b2c051d3073c5ad3abb8486e
husaynhakeem@gmail.com <husaynhakeem@gmail.com>
2018-04-21 19:42:29 -0700

    Chapter 8 exercise 4: Solution 1

--- src/chapter8_exercise3/java/Solution_followup_1.java
+++ src/chapter8_exercise3/java/Solution_followup_1.java
@@ -4,7 +4,6 @@ public class Solution_followup_1 {
 
     public static void main(String[] args) {
         int[] a = new int[]{-10, -5, 2, 2, 2, 3, 4, 7, 9, 12, 13};
-        //int[] a = new int[]{0, 0, 0, 3, 6};
         System.out.println(findMagicIndex(a));
     }
 
