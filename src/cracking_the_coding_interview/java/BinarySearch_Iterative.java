d67ba8deceab8dcfc04564b1b173ace7226bd0f3
husaynhakeem@gmail.com <husaynhakeem@gmail.com>
2018-09-18 02:24:42 -0700

    Cracking the coding interview - Chapter 10 - Binary search - iterative

--- src/cracking_the_coding_interview/java/BinarySearch_Iterative.java
+++ src/cracking_the_coding_interview/java/BinarySearch_Iterative.java
@@ -1,33 +1,25 @@
 package cracking_the_coding_interview.java;
 
-public class BinarySearch_Iterative {
+import org.jetbrains.annotations.NotNull;
 
-    public static void main(String[] args) {
-        int[] first = new int[]{1, 2, 3, 4, 7, 9, 10, 20, 168};
-        System.out.println(binarySearch(first, 20));
+class BinarySearch_Iterative {
 
-        int[] second = new int[]{1, 2, 3, 4, 7, 9, 10, 20, 168};
-        System.out.println(binarySearch(second, 5));
-    }
-
-    private static int binarySearch(int[] array, int x) {
-        if (array == null)
-            return -1;
-
+    int search(@NotNull final int[] array, final int x) {
         int low = 0;
         int high = array.length - 1;
         int mid;
 
         while (low <= high) {
-            mid = (low + high) / 2;
-
-            if (array[mid] == x)
+            mid = low + (high - low) / 2;
+            if (array[mid] == x) {
                 return mid;
+            }
 
-            if (array[mid] < x)
+            if (array[mid] < x) {
                 low = mid + 1;
-            else if (array[mid] > x)
+            } else {
                 high = mid - 1;
+            }
         }
 
         return -1;
---
+++ test/cracking_the_coding_interview/java/BinarySearch_IterativeTest.java
@@ -0,0 +1,40 @@
+package cracking_the_coding_interview.java;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BinarySearch_IterativeTest {
+
+    private final BinarySearch_Iterative binarySearch = new BinarySearch_Iterative();
+
+    @Test
+    public void search_1() throws Exception {
+        final int[] array = {1, 2, 3, 4, 7, 9, 10, 20, 168};
+
+        final int result = binarySearch.search(array, 20);
+
+        assertEquals(7, result);
+    }
+
+    @Test
+    public void search_2() throws Exception {
+        final int[] array = {1, 2, 3, 4, 7, 9, 10, 20, 168};
+
+        final int result = binarySearch.search(array, 5);
+
+        assertEquals(-1, result);
+    }
+
+    @Test
+    public void search_3() throws Exception {
+        final int[] array = new int[100_000_000];
+        for (int i = 0; i < 100_000_000; i++) {
+            array[i] = i;
+        }
+
+        final int result = binarySearch.search(array, 800_000);
+
+        assertEquals(800_000, result);
+    }
+}
