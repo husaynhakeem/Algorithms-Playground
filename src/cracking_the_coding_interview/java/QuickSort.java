fcf8d79fbdae88f4bae80e3d21ccc03351fee6b5
husaynhakeem@gmail.com <husaynhakeem@gmail.com>
2018-09-18 20:13:16 -0700

    Cracking the coding interview - Chapter 10 - Quick sort

--- src/cracking_the_coding_interview/java/QuickSort.java
+++ src/cracking_the_coding_interview/java/QuickSort.java
@@ -1,56 +1,41 @@
 package cracking_the_coding_interview.java;
 
-import static Utilities.java.ArrayUtils.printArrayOfInts;
+import org.jetbrains.annotations.NotNull;
 
-public class QuickSort {
+class QuickSort {
 
-    public static void main(String[] args) {
-        int[] first = new int[]{10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
-        quickSort(first);
-        printArrayOfInts(first);
-
-        int[] second = new int[]{5, 4, 6, 9, 1, 3, 2, 7, 0, 10, 8};
-        quickSort(second);
-        printArrayOfInts(second);
+    void sort(@NotNull final int[] array) {
+        sort(array, 0, array.length - 1);
     }
 
-    private static void quickSort(int[] array) {
-        if (array == null)
+    private void sort(int[] array, final int low, final int high) {
+        if (low >= high) {
             return;
-
-        quickSort(array, 0, array.length - 1);
+        }
+        final int index = partition(array, low, high);
+        sort(array, low, index - 1);
+        sort(array, index, high);
     }
 
-    private static void quickSort(int[] array, int low, int high) {
-        int index = partition(array, low, high);
-
-        if (low < index - 1)
-            quickSort(array, low, index - 1);
-
-        if (index < high)
-            quickSort(array, index, high);
-    }
-
-    private static int partition(int[] array, int low, int high) {
-        int pivot = array[(low + high) / 2];
-
+    private int partition(int[] array, int low, int high) {
+        final int pivot = array[low + (high - low) / 2];
         while (low <= high) {
-            while (array[low] < pivot)
+            while (array[low] < pivot) {
                 low++;
+            }
 
-            while (array[high] > pivot)
+            while (array[high] > pivot) {
                 high--;
+            }
 
             if (low <= high) {
-                int temp = array[low];
+                final int temp = array[low];
                 array[low] = array[high];
                 array[high] = temp;
-
                 low++;
                 high--;
             }
         }
-
         return low;
     }
 }
