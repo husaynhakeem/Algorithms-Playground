eafe9f6640d121ac60d715b618167fbe55ea7ee8
husaynhakeem@gmail.com <husaynhakeem@gmail.com>
2018-09-22 13:03:23 -0700

    Cracking the coding interview - Chapter 8 - Exercise 2 - Solution 3

---
+++ test/cracking_the_coding_interview/chapter_08/exercise_02/Solution_3Test.java
@@ -0,0 +1,72 @@
+package cracking_the_coding_interview.chapter_08.exercise_02;
+
+import cracking_the_coding_interview.chapter_08.exercise_02.Solution_3.Coordinates;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class Solution_3Test {
+
+    private final Solution_3 solution = new Solution_3();
+
+    @Test
+    public void robotInGrid_1() throws Exception {
+        final Coordinates coordinates00 = new Coordinates(0, 0);
+        final Coordinates[][] grid = {
+                {coordinates00}
+        };
+
+        final List<Coordinates> result = solution.robotInGrid(grid);
+
+        assertEquals(1, result.size());
+        assertEquals(coordinates00, result.get(0));
+    }
+
+    @Test
+    public void robotInGrid_2() throws Exception {
+        final Coordinates coordinates00 = new Coordinates(0, 0);
+        final Coordinates coordinates01 = new Coordinates(0, 1, true);
+        final Coordinates coordinates10 = new Coordinates(1, 0);
+        final Coordinates coordinates11 = new Coordinates(1, 1);
+        final Coordinates[][] grid = {
+                {coordinates00, coordinates01},
+                {coordinates10, coordinates11}
+        };
+
+        final List<Coordinates> result = solution.robotInGrid(grid);
+
+        assertEquals(3, result.size());
+        assertEquals(coordinates00, result.get(0));
+        assertEquals(coordinates10, result.get(1));
+        assertEquals(coordinates11, result.get(2));
+    }
+
+    @Test
+    public void robotInGrid_3() throws Exception {
+        final Coordinates coordinates00 = new Coordinates(0, 0);
+        final Coordinates coordinates01 = new Coordinates(0, 1);
+        final Coordinates coordinates02 = new Coordinates(0, 2);
+        final Coordinates coordinates10 = new Coordinates(1, 0, true);
+        final Coordinates coordinates11 = new Coordinates(1, 1);
+        final Coordinates coordinates12 = new Coordinates(1, 2, true);
+        final Coordinates coordinates20 = new Coordinates(2, 0);
+        final Coordinates coordinates21 = new Coordinates(2, 1);
+        final Coordinates coordinates22 = new Coordinates(2, 2);
+        final Coordinates[][] grid = {
+                {coordinates00, coordinates01, coordinates02},
+                {coordinates10, coordinates11, coordinates12},
+                {coordinates20, coordinates21, coordinates22}
+        };
+
+        final List<Coordinates> result = solution.robotInGrid(grid);
+
+        assertEquals(5, result.size());
+        assertEquals(coordinates00, result.get(0));
+        assertEquals(coordinates01, result.get(1));
+        assertEquals(coordinates11, result.get(2));
+        assertEquals(coordinates21, result.get(3));
+        assertEquals(coordinates22, result.get(4));
+    }
+}
