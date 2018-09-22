54269dbf8830f22038d05271d8d11840fbcb4f55
husaynhakeem@gmail.com <husaynhakeem@gmail.com>
2018-09-22 14:47:29 -0700

    Cracking the coding interview - Chapter 8 - Exercise 4 - Solution 1

--- src/cracking_the_coding_interview/chapter_08/exercise_04/Solution_1.java
+++ src/cracking_the_coding_interview/chapter_08/exercise_04/Solution_1.java
@@ -1,25 +1,25 @@
 package cracking_the_coding_interview.chapter_08.exercise_04;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class Solution_1 {
+class Solution_1 {
 
-    private static List<List<Integer>> getAllSubsets(List<Integer> set) {
-
+    List<List<Integer>> powerSet(@NotNull final List<Integer> list) {
         List<List<Integer>> level = new ArrayList<>();
         level.add(new ArrayList<>());
 
-        for (Integer n : set) {
+        for (Integer n : list) {
             level = addToPreviousLevel(n, level);
         }
 
         return level;
     }
 
-    private static List<List<Integer>> addToPreviousLevel(int n, List<List<Integer>> subsets) {
+    private List<List<Integer>> addToPreviousLevel(final int n, final List<List<Integer>> subsets) {
         List<List<Integer>> result = new ArrayList<>();
-
         result.addAll(subsets);
 
         for (List<Integer> subset : subsets) {
---
+++ test/cracking_the_coding_interview/chapter_08/exercise_04/Solution_1Test.java
@@ -0,0 +1,85 @@
+package cracking_the_coding_interview.chapter_08.exercise_04;
+
+import Utilities.java.ListUtils;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class Solution_1Test {
+
+    private final Solution_1 solution = new Solution_1();
+
+    @Test
+    public void powerSet_1() throws Exception {
+        final List<Integer> list = ListUtils.createList();
+
+        final List<List<Integer>> result = solution.powerSet(list);
+
+        final List<List<Integer>> expectedResult = ListUtils.createList(
+                ListUtils.createList()
+        );
+        assertEquals(1, result.size());
+        for (int i = 0; i < result.size(); i++) {
+            assertTrue(ListUtils.areEqual(expectedResult.get(i), result.get(i)));
+        }
+    }
+
+    @Test
+    public void powerSet_2() throws Exception {
+        final List<Integer> list = ListUtils.createList(1);
+
+        final List<List<Integer>> result = solution.powerSet(list);
+
+        final List<List<Integer>> expectedResult = ListUtils.createList(
+                ListUtils.createList(),
+                ListUtils.createList(1)
+        );
+        assertEquals(2, result.size());
+        for (int i = 0; i < result.size(); i++) {
+            assertTrue(ListUtils.areEqual(expectedResult.get(i), result.get(i)));
+        }
+    }
+
+    @Test
+    public void powerSet_3() throws Exception {
+        final List<Integer> list = ListUtils.createList(1, 2);
+
+        final List<List<Integer>> result = solution.powerSet(list);
+
+        final List<List<Integer>> expectedResult = ListUtils.createList(
+                ListUtils.createList(),
+                ListUtils.createList(1),
+                ListUtils.createList(2),
+                ListUtils.createList(1, 2)
+        );
+        assertEquals(4, result.size());
+        for (int i = 0; i < result.size(); i++) {
+            assertTrue(ListUtils.areEqual(expectedResult.get(i), result.get(i)));
+        }
+    }
+
+    @Test
+    public void powerSet_4() throws Exception {
+        final List<Integer> list = ListUtils.createList(1, 2, 3);
+
+        final List<List<Integer>> result = solution.powerSet(list);
+
+        final List<List<Integer>> expectedResult = ListUtils.createList(
+                ListUtils.createList(),
+                ListUtils.createList(1),
+                ListUtils.createList(2),
+                ListUtils.createList(1, 2),
+                ListUtils.createList(3),
+                ListUtils.createList(1, 3),
+                ListUtils.createList(2, 3),
+                ListUtils.createList(1, 2, 3)
+        );
+        assertEquals(8, result.size());
+        for (int i = 0; i < result.size(); i++) {
+            assertTrue(ListUtils.areEqual(expectedResult.get(i), result.get(i)));
+        }
+    }
+}
