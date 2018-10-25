40848dbf117ff9c6737f95fd3cbe1c907df3bbc5
husaynhakeem@gmail.com <husaynhakeem@gmail.com>
2018-10-24 19:41:17 -0700

    Big-O LA: Week 3 - Problem 2 - Solution 1

---
+++ src/big_o_la/week_03/problem_02/Solution_1.java
@@ -0,0 +1,48 @@
+package big_o_la.week_03.problem_02;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+class Solution_1 {
+
+    static class Interval {
+        int lowerBound;
+        int upperBound;
+
+        Interval(int lowerBound, int upperBound) {
+            this.lowerBound = lowerBound;
+            this.upperBound = upperBound;
+        }
+
+        boolean overlapsWith(final Interval interval) {
+            return this.lowerBound <= interval.lowerBound && interval.lowerBound <= this.upperBound;
+        }
+
+        Interval mergeWith(final Interval interval) {
+            return new Interval(
+                    Math.min(lowerBound, interval.lowerBound),
+                    Math.max(upperBound, interval.upperBound));
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (!(obj instanceof Interval)) {
+                return false;
+            }
+            final Interval interval = (Interval) obj;
+            return lowerBound == interval.lowerBound && upperBound == interval.upperBound;
+        }
+    }
+
+    List<Interval> mergeIntervals(final List<Interval> intervals) {
+        intervals.sort(Comparator.comparingInt(interval -> interval.lowerBound));
+        final List<Interval> result = new ArrayList<>();
+        for (int i = 0; i < intervals.size() - 1; i++) {
+            if (intervals.get(i).overlapsWith(intervals.get(i + 1))) {
+                result.add(intervals.get(i).mergeWith(intervals.get(i + 1)));
+            }
+        }
+        return result;
+    }
+}
---
+++ test/big_o_la/week_03/problem_02/Solution_1Test.java
@@ -0,0 +1,30 @@
+package big_o_la.week_03.problem_02;
+
+import Utilities.java.ListUtils;
+import big_o_la.week_03.problem_02.Solution_1.Interval;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class Solution_1Test {
+
+    private final Solution_1 solution = new Solution_1();
+
+    @Test
+    public void mergeIntervals_1() throws Exception {
+        final List<Interval> intervals = ListUtils.createList(
+                new Interval(2, 4),
+                new Interval(6, 8),
+                new Interval(5, 7),
+                new Interval(1, 3));
+
+        final List<Interval> result = solution.mergeIntervals(intervals);
+
+        assertEquals(2, result.size());
+        assertTrue(result.contains(new Interval(1, 4)));
+        assertTrue(result.contains(new Interval(5, 8)));
+    }
+}
