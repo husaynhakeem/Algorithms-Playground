ecd83dbca2a96013489158af40da2a271c1a68d7
husaynhakeem@gmail.com <husaynhakeem@gmail.com>
2018-10-28 12:53:48 -0700

    Leetcode: Problem 215- Solution 1

---
+++ src/leetcode/problem_0215/Solution_1.java
@@ -0,0 +1,33 @@
+package leetcode.problem_0215;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution_1 {
+
+    public int findKthLargest(int[] array, int k) {
+        final Set<Integer> indicesToIgnore = new HashSet<>();
+        int max = 0;
+
+        for (int i = 0; i < k; i++) {
+            max = getMaxHelper(array, indicesToIgnore);
+        }
+
+        return max;
+    }
+
+    private int getMaxHelper(final int[] array, final Set<Integer> indicesToIgnore) {
+        int max = Integer.MIN_VALUE;
+        int indexMax = Integer.MIN_VALUE;
+
+        for (int i = 0; i < array.length; i++) {
+            if (!indicesToIgnore.contains(i) && array[i] > max) {
+                max = array[i];
+                indexMax = i;
+            }
+        }
+
+        indicesToIgnore.add(indexMax);
+        return max;
+    }
+}
