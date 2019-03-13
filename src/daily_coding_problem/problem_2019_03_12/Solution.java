75ff03945734c2279e17c064351d190f05106233
husaynhakeem@gmail.com <husaynhakeem@gmail.com>
2019-03-12 18:39:28 -0700

    Daily coding problem: 2019-03-12

---
+++ src/daily_coding_problem/problem_2019_03_12/Solution.java
@@ -0,0 +1,20 @@
+package daily_coding_problem.problem_2019_03_12;
+
+public class Solution {
+
+    int[] productExceptSelf(final int[] array) {
+        productExceptSelfHelper(0, 1, array);
+        return array;
+    }
+
+    private int productExceptSelfHelper(final int index, final int productBefore, final int[] array) {
+        if (index >= array.length) {
+            return 1;
+        }
+
+        final int currentValue = array[index];
+        final int productAfter = productExceptSelfHelper(index + 1, productBefore * currentValue, array);
+        array[index] = productBefore * productAfter;
+        return currentValue * productAfter;
+    }
+}
