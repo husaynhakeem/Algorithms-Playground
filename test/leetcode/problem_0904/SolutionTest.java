d50e48c0380b769181a609052521cc0e8b31b6ed
husaynhakeem@gmail.com <husaynhakeem@gmail.com>
2019-03-20 19:17:45 -0700

    Leetcode problem 904

---
+++ test/leetcode/problem_0904/SolutionTest.java
@@ -0,0 +1,37 @@
+package leetcode.problem_0904;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+
+    private final Solution solution = new Solution();
+
+    @Test
+    public void totalFruit_1() throws Exception {
+        final int[] tree = {1, 0, 1, 4, 1, 4, 1, 2, 3};
+
+        final int result = solution.totalFruit(tree);
+
+        assertEquals(5, result);
+    }
+
+    @Test
+    public void totalFruit_2() throws Exception {
+        final int[] tree = {1, 2, 1};
+
+        final int result = solution.totalFruit(tree);
+
+        assertEquals(3, result);
+    }
+
+    @Test
+    public void totalFruit_3() throws Exception {
+        final int[] tree = {0, 1, 2, 2};
+
+        final int result = solution.totalFruit(tree);
+
+        assertEquals(3, result);
+    }
+}
