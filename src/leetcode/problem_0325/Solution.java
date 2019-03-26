ff137738f6e65edda3f3c05f6c9cb7334ecff02d
husaynhakeem@gmail.com <husaynhakeem@gmail.com>
2019-03-25 21:16:52 -0700

    Leetcode problem 325

---
+++ src/leetcode/problem_0325/Solution.java
@@ -0,0 +1,21 @@
+package leetcode.problem_0325;
+
+public class Solution {
+
+    int maxSubArrayLen(int[] nums, int k) {
+        int max = 0;
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum = 0;
+            for (int j = i; j < nums.length; j++) {
+                sum += nums[j];
+                if (sum == k) {
+                    max = Math.max(max, j - i + 1);
+                }
+            }
+        }
+
+        return max;
+    }
+}
---
+++ test/leetcode/problem_0325/SolutionTest.java
@@ -0,0 +1,30 @@
+package leetcode.problem_0325;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+
+    private final Solution solution = new Solution();
+
+    @Test
+    public void maxSubArrayLen_1() throws Exception {
+        final int[] nums = {1, -1, 5, -2, 3};
+        final int k = 3;
+
+        final int result = solution.maxSubArrayLen(nums, k);
+
+        assertEquals(4, result);
+    }
+
+    @Test
+    public void maxSubArrayLen_2() throws Exception {
+        final int[] nums = {-2, -1, 2, 1};
+        final int k = 1;
+
+        final int result = solution.maxSubArrayLen(nums, k);
+
+        assertEquals(2, result);
+    }
+}
