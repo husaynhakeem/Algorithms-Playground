58a09d8417792288b3ab30ae440825d763f24ce1
husaynhakeem@gmail.com <husaynhakeem@gmail.com>
2019-04-01 19:03:53 -0700

    Leetcode microsoft interview 2 - Problem 2

---
+++ src/leetcode/interview_microsoft_2/problem_2/Solution.java
@@ -0,0 +1,27 @@
+package leetcode.interview_microsoft_2.problem_2;
+
+public class Solution {
+
+    int compareVersion(String version1, String version2) {
+        final String[] c1 = version1.split("\\.");
+        final String[] c2 = version2.split("\\.");
+
+        int index = 0;
+        while (index < c1.length || index < c2.length) {
+            final int r1 = index >= c1.length ? 0 : Integer.parseInt(c1[index]);
+            final int r2 = index >= c2.length ? 0 : Integer.parseInt(c2[index]);
+
+            if (r1 > r2) {
+                return 1;
+            }
+
+            if (r1 < r2) {
+                return -1;
+            }
+
+            index++;
+        }
+
+        return 0;
+    }
+}
