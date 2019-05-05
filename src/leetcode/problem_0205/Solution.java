f9bfc721b3d1e772a364381536aa8df9fa319bc5
husaynhakeem@gmail.com <husaynhakeem@gmail.com>
2019-05-05 00:04:16 -0700

    Leetcode problem 205

---
+++ src/leetcode/problem_0205/Solution.java
@@ -0,0 +1,30 @@
+package leetcode.problem_0205;
+
+import java.util.Hashtable;
+
+public class Solution {
+
+    boolean isIsomorphic(String s, String t) {
+        Hashtable<Character, Character> mappings = new Hashtable<>();
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+
+        for (int i = 0; i < s.length(); i++) {
+            char sChar = sChars[i];
+            char tChar = tChars[i];
+            Character mapping = mappings.get(sChar);
+
+            if (mapping == null) {
+                if (mappings.containsValue(tChar)) {
+                    return false;
+                } else {
+                    mappings.put(sChar, tChar);
+                }
+            } else if (mapping != tChar) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
