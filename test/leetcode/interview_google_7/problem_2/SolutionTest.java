90fa117cba52b0a3efa0a1e8d097bc172bc0e10a
husaynhakeem@gmail.com <husaynhakeem@gmail.com>
2019-05-05 14:59:48 -0700

    Google interview 7 - Problem 2

--- test/leetcode/interview_google_7/problem_2/SolutionTest.java
+++ test/leetcode/interview_google_7/problem_2/SolutionTest.java
@@ -1,5 +1,36 @@
-import static org.junit.Assert.*;
+package leetcode.interview_google_7.problem_2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class SolutionTest {
 
+    @Test
+    public void examRoom_1() throws Exception {
+        final Solution.ExamRoom examRoom = new Solution.ExamRoom(10);
+        assertEquals(0, examRoom.seat());
+        assertEquals(9, examRoom.seat());
+        assertEquals(4, examRoom.seat());
+        assertEquals(2, examRoom.seat());
+        examRoom.leave(4);
+        assertEquals(5, examRoom.seat());
+    }
+
+    @Test
+    public void examRoom_2() throws Exception {
+        final Solution.ExamRoom examRoom = new Solution.ExamRoom(8);
+        assertEquals(0, examRoom.seat());
+        assertEquals(7, examRoom.seat());
+        assertEquals(3, examRoom.seat());
+        examRoom.leave(0);
+        examRoom.leave(7);
+        assertEquals(7, examRoom.seat());
+        assertEquals(0, examRoom.seat());
+        assertEquals(5, examRoom.seat());
+        assertEquals(1, examRoom.seat());
+        assertEquals(2, examRoom.seat());
+        assertEquals(4, examRoom.seat());
+        assertEquals(6, examRoom.seat());
+    }
 }
