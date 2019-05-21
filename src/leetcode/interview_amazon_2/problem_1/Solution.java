36a4cafa2ff024e6eba632686d9bc0d72ce17c5f
husaynhakeem@gmail.com <husaynhakeem@gmail.com>
2019-05-20 18:32:59 -0700

    Leetcode interview amazon 2 - Problem 1

---
+++ src/leetcode/interview_amazon_2/problem_1/Solution.java
@@ -0,0 +1,16 @@
+package leetcode.interview_amazon_2.problem_1;
+
+public class Solution {
+
+    public int maxProfit(final int[] prices) {
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                if (prices[j] - prices[i] > maxProfit) {
+                    maxProfit = prices[j] - prices[i];
+                }
+            }
+        }
+        return maxProfit;
+    }
+}
