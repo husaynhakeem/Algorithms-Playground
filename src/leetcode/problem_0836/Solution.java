ef4937b7bef8e6a6fb8caec7c508a3cc3eb01936
husaynhakeem@gmail.com <husaynhakeem@gmail.com>
2019-05-21 22:15:36 -0700

    Leetcode problem 836

---
+++ src/leetcode/problem_0836/Solution.java
@@ -0,0 +1,9 @@
+package leetcode.problem_0836;
+
+public class Solution {
+
+    boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        return rec1[0] < rec2[2] && rec1[2] > rec2[0] &&
+                rec1[1] < rec2[3] && rec1[3] > rec2[1];
+    }
+}
