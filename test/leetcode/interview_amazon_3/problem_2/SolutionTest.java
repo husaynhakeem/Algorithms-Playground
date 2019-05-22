47bb7eda45552bc6c94dd8c8a9baf80dc0be385e
husaynhakeem@gmail.com <husaynhakeem@gmail.com>
2019-05-21 22:18:27 -0700

    Leetcode interview amazon 3 - Problem 2

---
+++ test/leetcode/interview_amazon_3/problem_2/SolutionTest.java
@@ -0,0 +1,22 @@
+package leetcode.interview_amazon_3.problem_2;
+
+import Utilities.java.ListUtils;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+public class SolutionTest {
+
+    private final Solution solution = new Solution();
+
+    @Test
+    public void partitionLabels() throws Exception {
+        final String s = "ababcbacadefegdehijhklij";
+
+        final List<Integer> result = solution.partitionLabels(s);
+
+        assertTrue(ListUtils.areEqual(ListUtils.createList(9, 7, 8), result));
+    }
+}
