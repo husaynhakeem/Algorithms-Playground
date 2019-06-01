42c8c691ecb99bcf50f04c727b30514eaaaa012c
Husayn Hakeem <husaynhakeem@gmail.com>
2019-05-31 19:36:08 -0700

    Leetcode problem 97

---
+++ test/leetcode/problem_0097/SolutionTest.java
@@ -0,0 +1,33 @@
+package leetcode.problem_0097;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class SolutionTest {
+
+    private final Solution solution = new Solution();
+
+    @Test
+    public void isInterleave_1() {
+        final String s1 = "aabcc";
+        final String s2 = "dbbca";
+        final String s3 = "aadbbcbcac";
+
+        final boolean result = solution.isInterleave(s1, s2, s3);
+
+        assertTrue(result);
+    }
+
+    @Test
+    public void isInterleave_2() {
+        final String s1 = "aabcc";
+        final String s2 = "dbbca";
+        final String s3 = "aadbbbaccc";
+
+        final boolean result = solution.isInterleave(s1, s2, s3);
+
+        assertFalse(result);
+    }
+}
