8158bd25b9540890e543fab5bb9fe24bdce3ea82
Husayn Hakeem <husaynhakeem@gmail.com>
2020-11-01 20:31:43 -0800

    CTCI chapter 1 problem 7

--- src/cracking_the_coding_interview/chapter_01/exercise_07/SolutionFollowUp.java
+++ src/cracking_the_coding_interview/chapter_01/exercise_07/SolutionFollowUp.java
@@ -4,15 +4,31 @@ import org.jetbrains.annotations.NotNull;
 
 class SolutionFollowUp {
 
+    @SuppressWarnings("UnnecessaryLocalVariable")
     void rotateMatrix(@NotNull final int[][] matrix) {
         final int N = matrix.length;
-        for (int first = 0; first < N / 2; first++) {
-            int last = N - 1 - first;
+        for (int layer = 0; layer < N / 2; layer++) {
+
+            final int first = layer;
+            final int last = N - 1 - first;
+
             for (int j = first; j < last; j++) {
+
+                final int offset = j - first;
+
+                // Store top element
                 int temp = matrix[first][j];
-                matrix[first][j] = matrix[j][first];
-                matrix[j][first] = matrix[last][j];
-                matrix[last][j] = matrix[j][last];
+
+                // Top <- left
+                matrix[first][j] = matrix[last - offset][first];
+
+                // Left <- bottom
+                matrix[last - offset][first] = matrix[last][last - offset];
+
+                // Bottom <- right
+                matrix[last][last - offset] = matrix[j][last];
+
+                // Right <- Top
                 matrix[j][last] = temp;
             }
         }
--- test/cracking_the_coding_interview/chapter_01/exercise_07/SolutionFollowUpTest.java
+++ test/cracking_the_coding_interview/chapter_01/exercise_07/SolutionFollowUpTest.java
@@ -10,37 +10,55 @@ public class SolutionFollowUpTest {
     private final SolutionFollowUp solution = new SolutionFollowUp();
 
     @Test
-    public void rotateMatrix_1() throws Exception {
+    public void rotateMatrix_1() {
         final int[][] matrix = {
-                {0, 1},
-                {2, 3}
+                {1, 2},
+                {3, 4}
         };
 
         solution.rotateMatrix(matrix);
 
         final int[][] expectedResult = {
-                {2, 0},
-                {3, 1}
+                {3, 1},
+                {4, 2}
         };
         assertTrue(GridUtils.areIntegerGridsEqual(expectedResult, matrix));
     }
 
     @Test
-    public void rotateMatrix_2() throws Exception {
+    public void rotateMatrix_2() {
         final int[][] matrix = {
-                {0, 1, 2, 3},
-                {4, 5, 6, 7},
-                {8, 9, 10, 11},
-                {12, 13, 14, 15}
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
         };
 
         solution.rotateMatrix(matrix);
 
         final int[][] expectedResult = {
-                {12, 8, 4, 0},
+                {7, 4, 1},
+                {8, 5, 2},
+                {9, 6, 3}
+        };
+        assertTrue(GridUtils.areIntegerGridsEqual(expectedResult, matrix));
+    }
+
+    @Test
+    public void rotateMatrix_3() {
+        final int[][] matrix = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10,11, 12},
+                {13, 14, 15, 16},
+        };
+
+        solution.rotateMatrix(matrix);
+
+        final int[][] expectedResult = {
                 {13, 9, 5, 1},
                 {14, 10, 6, 2},
-                {15, 11, 7, 3}
+                {15, 11, 7, 3},
+                {16, 12, 8, 4},
         };
         assertTrue(GridUtils.areIntegerGridsEqual(expectedResult, matrix));
     }
