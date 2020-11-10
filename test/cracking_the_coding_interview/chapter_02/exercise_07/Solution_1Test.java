b145cb7b7611957e6b89a9120cb1778092c53d96
Husayn Hakeem <husaynhakeem@gmail.com>
2020-11-09 20:21:57 -0800

    CTCI chapter 2 problem 7

--- test/cracking_the_coding_interview/chapter_02/exercise_07/Solution_1Test.java
+++ test/cracking_the_coding_interview/chapter_02/exercise_07/Solution_1Test.java
@@ -1,5 +1,6 @@
 package cracking_the_coding_interview.chapter_02.exercise_07;
 
+import cracking_the_coding_interview.chapter_02.exercise_07.Solution_1.Node;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -10,41 +11,40 @@ public class Solution_1Test {
     private final Solution_1 solution = new Solution_1();
 
     @Test
-    public void intersection_1() throws Exception {
-        final Solution_1.Node commonNode = new Solution_1.Node(2);
+    public void intersection_1() {
+        final Node commonNode = new Node(2);
+        commonNode.next = new Node(4);
+        commonNode.next.next = new Node(5);
 
-        final Solution_1.Node l1 = new Solution_1.Node(1);
-        l1.next = new Solution_1.Node(2);
-        l1.next.next = new Solution_1.Node(3);
-        l1.next.next.next = new Solution_1.Node(4);
+        final Node l1 = new Node(1);
+        l1.next = new Node(2);
+        l1.next.next = new Node(3);
+        l1.next.next.next = new Node(4);
         l1.next.next.next.next = commonNode;
-        l1.next.next.next.next.next = new Solution_1.Node(5);
 
-        final Solution_1.Node l2 = new Solution_1.Node(1);
-        l2.next = new Solution_1.Node(2);
+        final Node l2 = new Node(1);
+        l2.next = new Node(2);
         l2.next.next = commonNode;
-        l2.next.next.next = new Solution_1.Node(4);
-        l2.next.next.next.next = new Solution_1.Node(5);
 
-        final Solution_1.Node result = solution.intersection(l1, l2);
+        final Node result = solution.intersection(l1, l2);
 
         assertEquals(commonNode, result);
     }
 
     @Test
-    public void intersection_2() throws Exception {
-        final Solution_1.Node l1 = new Solution_1.Node(1);
-        l1.next = new Solution_1.Node(2);
-        l1.next.next = new Solution_1.Node(3);
-        l1.next.next.next = new Solution_1.Node(4);
-        l1.next.next.next.next = new Solution_1.Node(5);
+    public void intersection_2() {
+        final Node l1 = new Node(1);
+        l1.next = new Node(2);
+        l1.next.next = new Node(3);
+        l1.next.next.next = new Node(4);
+        l1.next.next.next.next = new Node(5);
 
-        final Solution_1.Node l2 = new Solution_1.Node(1);
-        l2.next = new Solution_1.Node(2);
-        l2.next.next = new Solution_1.Node(4);
-        l2.next.next.next = new Solution_1.Node(5);
+        final Node l2 = new Node(1);
+        l2.next = new Node(2);
+        l2.next.next = new Node(4);
+        l2.next.next.next = new Node(5);
 
-        final Solution_1.Node result = solution.intersection(l1, l2);
+        final Node result = solution.intersection(l1, l2);
 
         assertNull(result);
     }
